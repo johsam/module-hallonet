e0cc897d779d999656de37a6fc1ab270f8e5199f
johsam <j.samuelson64@gmail.com>
2015-04-03 13:03:14 +0200

    Added proper exit

--- files/pi/rfx-commands/commands/nexa_one_off.sh
+++ files/pi/rfx-commands/commands/nexa_one_off.sh
@@ -7,3 +7,5 @@ ipaddress=$(/usr/bin/facter ipaddress)
 printf "$(date '+%F %T')\tsend\t(${1} -> off)\n" >> /var/rfxcmd/nexa-setstate.log
 
 /opt/rfxcmd/rfxsend.py -s ${ipaddress} -r "0B11000000D813320${1}000000"
+
+exit 0
--- files/pi/rfx-commands/commands/nexa_one_on.sh
+++ files/pi/rfx-commands/commands/nexa_one_on.sh
@@ -7,3 +7,5 @@ ipaddress=$(/usr/bin/facter ipaddress)
 printf "$(date '+%F %T')\tsend\t(${1} -> on)\n" >> /var/rfxcmd/nexa-setstate.log
 
 /opt/rfxcmd/rfxsend.py -s ${ipaddress} -r "0B11000000D813320${1}010F00"
+
+exit 0
