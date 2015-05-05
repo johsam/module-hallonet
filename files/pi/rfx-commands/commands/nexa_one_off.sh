cc35221e60493aff0ae3ce380faa9e7cd9c10ee6
johsam <j.samuelson64@gmail.com>
2015-05-05 20:16:22 +0200

    Now using the log function from functions.sh

--- files/pi/rfx-commands/commands/nexa_one_off.sh
+++ files/pi/rfx-commands/commands/nexa_one_off.sh
@@ -1,10 +1,40 @@
 #!/bin/bash
 
+######################################################################
+#
+#	Catch signals...
+#
+######################################################################
+
+trap 'rm -f "${tmpfile}" > /dev/null 2>&1' 0
+trap "exit 2" 1 2 3 15
+
+######################################################################
+#
+#	Setup variables...
+#
+######################################################################
+
+[ -h "$0" ] && dir=$(dirname `readlink $0`) || dir=$( cd `dirname $0` && pwd)
+
+tmpfile="/tmp/`basename $0`-$$.tmp"
+
+settings=${dir}/../settings.cfg
+functions=${dir}/../functions.sh
+
+# Sanity checks
+
+[ -r ${settings} ]  && source ${settings}  || (echo "FATAL: Missing '${settings}', Aborting" ; exit 1)
+[ -r ${functions} ] && source ${functions} || (echo "FATAL: Missing '${functions}', Aborting" ; exit 1)
+
+
+# Do it
+
 umask 0011
 
 ipaddress=127.0.0.1
 
-printf "$(date '+%F %T')\tsend\t(${1} -> off)\n" >> /var/rfxcmd/nexa-setstate.log
+log "send  (${1} -> off)" >> /var/rfxcmd/nexa-setstate.log
 
 /opt/rfxcmd/rfxsend.py -s ${ipaddress} -r "0B11000000D813320${1}000000"
 
--- files/pi/rfx-commands/commands/nexa_setstate.sh
+++ files/pi/rfx-commands/commands/nexa_setstate.sh
@@ -1,8 +1,39 @@
 #!/bin/bash
 
+######################################################################
+#
+#	Catch signals...
+#
+######################################################################
+
+trap 'rm -f "${tmpfile}" > /dev/null 2>&1' 0
+trap "exit 2" 1 2 3 15
+
+######################################################################
+#
+#	Setup variables...
+#
+######################################################################
+
+[ -h "$0" ] && dir=$(dirname `readlink $0`) || dir=$( cd `dirname $0` && pwd)
+
+tmpfile="/tmp/`basename $0`-$$.tmp"
+
+settings=${dir}/../settings.cfg
+functions=${dir}/../functions.sh
+
+# Sanity checks
+
+[ -r ${settings} ]  && source ${settings}  || (echo "FATAL: Missing '${settings}', Aborting" ; exit 1)
+[ -r ${functions} ] && source ${functions} || (echo "FATAL: Missing '${functions}', Aborting" ; exit 1)
+
+
+# Do it
+
 umask 0011
 
-printf "$(date '+%F %T')\tstate\t($1 -> $2)\n" >> /var/rfxcmd/nexa-setstate.log
+log "state ($1 -> $2)" >> /var/rfxcmd/nexa-setstate.log
+
 
 id=$1
 onOff="$(echo "$2" | tr '[:lower:]' '[:upper:]')"
