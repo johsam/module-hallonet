1012a2327e78ebb192bf021ee90f2236a6c0b4df
johsam <j.samuelson64@gmail.com>
2017-04-17 09:20:46 +0200

    Sunrise and sunset are now calculated in python

--- files/pi/rfx-commands/scripts/update-sun-rise-set.sh
+++ files/pi/rfx-commands/scripts/update-sun-rise-set.sh
@@ -18,7 +18,6 @@ trap "exit 2" 1 2 3 15
 tmpfile="/tmp/`basename $0`-$$.tmp"
 
 
-
 [ -h "$0" ] && scriptDir=$(dirname `readlink $0`) || scriptDir=$( cd `dirname $0` && pwd)
 
 functions=${scriptDir}/../functions.sh
@@ -29,9 +28,30 @@ settings=${scriptDir}/../settings.cfg
 [ -r ${functions} ] && source ${functions} || { logger -t $(basename $0) "FATAL: Missing '${functions}', Aborting" ; exit 1; }
 [ -r ${settings} ]  && source ${settings}  || { logger -t $(basename $0) "FATAL: Missing '${settings}', Aborting" ; exit 1; }
 
-today=$(date +"%F")
-rise_set=$(/usr/local/bin/sun-rise-set.pl --longitude ${RIPAN_LON} --latitude ${RIPAN_LAT} -f "%H:%M" | tr  '\n' ' ')
+export RIPAN_LON RIPAN_LAT RIPAN_ALT
 
-echo "${today} ${rise_set}" >> /var/rfxcmd/sun-rise-set.log
+# Now we do it in python instead of perl
+
+python -<< END >> /var/rfxcmd/sun-rise-set.log
+import datetime
+import astral
+import os
+
+today = datetime.date.today()
+longitude = float(os.environ['RIPAN_LON'])
+latitude = float(os.environ['RIPAN_LAT'])
+altitude = float(os.environ['RIPAN_ALT'])
+
+mylocation = astral.Location(info=("Myplace", "Mycountry", latitude, longitude, "Europe/Stockholm", altitude))
+mylocation.solar_depression = "civil"
+
+result = mylocation.sun(date=today)
+
+print "{0} {1} {2}".format(
+    today.strftime("%Y-%m-%d"),
+    result['sunrise'].strftime("%H:%M"),
+    result['sunset'].strftime("%H:%M")
+)
+END
 
 exit 0
